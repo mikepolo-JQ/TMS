407bbe55a3a239781a7413cdbd27d44a47811853
mikepolo-JQ <52786686+mikepolo-JQ@users.noreply.github.com>
2020-10-16 21:56:28 +0300

    First TMS commite (#1)

--- src/framework/consts.py
+++ src/framework/consts.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 SERVER_RUNNING_BANNER = """
 +----------------------------------------+
 |             SERVER WORKS!              |
@@ -7,3 +9,14 @@ Visit http://{host}:{port}
 
 ..........................................
 """
+
+DIR_STATIC = (Path(__file__).parent.parent / "static").resolve()
+# _this_file_path = Path(__file__).resolve()
+#
+# DIR_FRAMEWORK = _this_file_path.parent.resolve()
+#
+# DIR_SRC = DIR_FRAMEWORK.parent.resolve()
+#
+# DIR_REPO = DIR_SRC.parent.resolve()
+#
+# DIR_STATIC = (DIR_REPO / "static").resolve()
