381a2dd69a0c929e2a7cd601bd1c34e29a0f9844
Misha Polozkov <polozkov1450@gmail.com>
2020-10-21 16:11:37 +0300

    add logo.png and mimetypes

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,3 +1,5 @@
+import mimetypes
+
 from framework.consts import DIR_STATIC
 
 
@@ -5,22 +7,34 @@ def application(environ, start_response):
     url = environ["PATH_INFO"]
     if url == "/new_list/":
         status = "200 OK"
+        text_css_content_type = mimetypes.guess_type("text.css")
         headers = {
-            "Content-type": "text/css",
+            "Content-type": text_css_content_type[0],
         }
         payload = read_from_styles_css()
         start_response(status, list(headers.items()))
 
         yield payload
+
+    elif url == "/logo.png/":
+        status = "200 OK"
+        logo_content_type = mimetypes.guess_type("logo.png")
+        headers = {
+            "Content-type": logo_content_type[0],
+        }
+        payload = read_from_logo_png()
+        start_response(status, list(headers.items()))
+        yield payload
+
     else:
         status = "200 OK"
+        text_html_content_type = mimetypes.guess_type("text.html")
         headers = {
-            "Content-type": "text/html",
+            "Content-type": text_html_content_type[0],
         }
         payload = read_from_index_html()
 
         start_response(status, list(headers.items()))
-
         yield payload
 
 
@@ -41,4 +55,13 @@ def read_from_styles_css():
         payload = fp.read()
 
     payload = payload.encode()
+    return payload
+
+
+def read_from_logo_png():
+    path = DIR_STATIC / "logo.png"
+
+    with path.open("rb") as fp:
+        payload = fp.read()
+
     return payload
--- tests/functional/test_app_main.py
+++ tests/functional/test_app_main.py
@@ -23,7 +23,7 @@ def validate_content(page: MainPage):
     assert page.h1.tag_name == "h1"
     assert page.h1.text == "My study on TeachMeSkills"
     assert page.p.tag_name == "p"
-    assert page.p.text == "This is dfnjknfdkjfndkjnfdjbngk"
+    assert page.p.text == "This is text"
 
     html = page.html
     assert "<hr>" in html
