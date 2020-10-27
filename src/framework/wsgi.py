876e12f901b78f8aa6015426c9a02d0a1afb0bf8
Misha Polozkov <polozkov1450@gmail.com>
2020-10-27 20:50:40 +0300

    new wsgi

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,4 +1,3 @@
-import random
 from mimetypes import guess_type
 
 from framework.consts import DIR_STATIC
@@ -7,24 +6,67 @@ from framework.consts import DIR_STATIC
 def application(environ, start_response):
     url = environ["PATH_INFO"]
 
-    file_names = {
-        "/styles": "assets/styles.css",
-        "/logo/": "logo.png",
-        "/": "index.html",
-        "/favicon.ico": "favicon.ico",
-        "/style_404": "assets/style_404.css",
-        "/404": "page_not_found.html",
+    handlers = {
+        "/": handler_index,
+        # "/styles": "assets/styles.css",
+        # "/logo/": "logo.png",
+        # "/favicon.ico": "favicon.ico",
+        # "/style_404": "assets/style_404.css",
+        # "/404": "page_not_found.html",
     }
 
-    file_name = file_names.get(url, "page_not_found.html")
-    status = generate_status(file_name)
+    handler = handlers.get(url, handler_404)
+    status = "200 OK"
     headers = {
-        "Content-type": guess_type(file_name)[0],
+        "Content-type": "text/html",
     }
-    payload = read_static(file_name)
+    payload = handler(environ)
 
     start_response(status, list(headers.items()))
     yield payload
+
+
+def handler_index(_environ) -> bytes:
+    base_html = read_static("_base.html").decode()
+    index_html = read_static("index.html").decode()
+    result = base_html.format(body=index_html)
+
+    return result.encode()
+
+
+def handler_404(environ) -> bytes:
+    url = environ["PATH_INFO"]
+    base_html = read_static("_base.html").decode()
+    body_404 = f"""
+            <div class="header">
+                <div class="container">
+                    <div class="header__inner">
+                        <div class="header__logo">MiPo</div>
+        
+                        <nav class="nav">
+                            <a class="nav__link" href="#">About</a>
+                            <a class="nav__link" href="#">Service</a>
+                            <a class="nav__link" href="#">Work</a>
+                            <a class="nav__link" href="#">Block</a>
+                            <a class="nav__link" href="#">Contact</a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="intro">
+                <div class="container">
+                    <div class="intro_inner">
+                        <h2 class="intro__suptitle">Ooops...</h2>
+                        <h1 class="intro__title">Your path {url} not found</h1>
+        
+                        <a href="/" class="button">< Go home</a>
+                    </div>
+                </div>
+        
+            </div>"""
+    result = base_html.format(body=str(body_404))
+    return result.encode()
 
 
 def read_static(file_name: str) -> bytes:
@@ -36,15 +78,14 @@ def read_static(file_name: str) -> bytes:
     return payload
 
 
-def generate_status(file_name: str) -> str:
-    if file_name == "page_not_found.html":
-        return "404 NOT FOUND"
-    return "200 OK"
+def generate_status() -> str:
+    pass
 
 
 # def generate_404(environ) -> bytes:
 #
 #     url = environ["PATH_INFO"]
 #     pin = random.randint(1, 1000)
+#
 #     msg = f"Hello, bro. Your path: {url} is not found. Pin: {pin}"
 #     return msg.encode()
