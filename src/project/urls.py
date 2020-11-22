d6c226ba23cc7d131c4ec5c9b35c5844a4263dc9
Misha Polozkov <polozkov1450@gmail.com>
2020-11-23 00:53:46 +0300

    add 404page & update views

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,54 +1,54 @@
+from random import randint
+
 from django.contrib import admin
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
 
-from framework.utils import read_static
-
-# from my_app import views
-
 
 def index(request):
-    payload = render(
-        request, "index.html", context={"styles": "{% static 'styles/styles.css' %}"}
-    )
+    payload = render(request, "index.html", context={"page": "index"})
     return HttpResponse(payload)
 
 
-def styles(_request):
-    payload = read_static("assets/styles/styles.css").content
-
-    return HttpResponse(payload, content_type="text/css")
-
-
-def hello(_request: HttpRequest) -> HttpResponse:
-    base = read_static("_base.html")
-    base_html = base.content.decode()
-    hello_html = read_static("hello.html").content.decode()
-
-    document = hello_html.format(
-        name_handler="Anon",
-        name_value="",
-        address_handler="XZ",
-        address_value="",
+def hello(request: HttpRequest) -> HttpResponse:
+    payload = render(
+        request,
+        "hello.html",
+        context={
+            "page": "hello",
+            "name_handler": "Anon",
+            "name_value": "",
+            "address_handler": "XZ",
+            "address_value": "",
+        },
     )
 
-    payload = base_html.format(
-        favicon="favicon.ico", styles="hello_styles.css/", body=document
-    ).encode()
     return HttpResponse(content=payload)
 
 
-def hello_styles(_request: HttpRequest) -> HttpResponse:
-    payload = read_static("assets/styles/hello_styles.css").content.decode()
+def not_found(request: HttpRequest) -> HttpResponse:
 
-    return HttpResponse(payload.encode(), content_type="text/css")
+    url = request.path
+    pin = randint(1, 1000)
+
+    payload = render(
+        request,
+        "not_found.html",
+        context={
+            "page": "not_found",
+            "url": url,
+            "pin": pin,
+            "request_headers": request.headers,
+        },
+    )
+    return HttpResponse(payload, status=404)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
     path("", index, name="index"),
     path("hello/", hello, name="hello"),
-    path("hello/s/assets/styles/hello_styles.css/", hello_styles),
+    path("learnMore/", not_found, name="not_found"),
 ]
