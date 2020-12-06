bdd55bc220b59a6254cd877cd0d76a8db9f7384b
Misha Polozkov <polozkov1450@gmail.com>
2020-12-06 23:27:33 +0300

    custom change

--- src/applications/hello/urls.py
+++ src/applications/hello/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from applications.hello.views import  HelloView
+from applications.hello.views import HelloView
 # from applications.hello.views import view_hello_greet
 # from applications.hello.views import view_hello_reset
 
