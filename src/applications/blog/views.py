fd06f6b8de6871158efd0d8c971f9cf5b98adde2
Misha Polozkov <polozkov1450@gmail.com>
2020-12-11 17:26:23 +0300

    add page post update & static upd

--- src/applications/blog/views.py
+++ src/applications/blog/views.py
@@ -48,6 +48,17 @@ class SinglePost(DetailView):
         return context
 
 
+class SingleUpdate(DetailView):
+    model = Post
+    template_name = "blog/post_update.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+
+        context.update({"ico": "g", "page": "blog"})
+        return context
+
+
 class UpdatePost(UpdateView):
     model = Post
     fields = ["content", "title"]
